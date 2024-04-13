f4136d473d1e5b1477c3bd9f22997a77d06acf8e
rice-b <80572042+rice-b@users.noreply.github.com>
2024-04-13 15:46:09 +0100

    Update contact.js

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,30 +1,14 @@
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
-import { facalendar-check } from '@fortawesome/free-brands-svg-icons/facalendar-check';
+import { fa-calendar-check } from '@fortawesome/free-brands-svg-icons/fa-calendar-check';
 // import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 // import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
 // import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
 // import { faAngellist } from '@fortawesome/free-brands-svg-icons/faAngellist';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
-
 const data = [
-/*   {
-    link: 'https://github.com/mldangelo',
-    label: 'Github',
-    icon: faGithub,
-  },
-  {
-    link: 'https://facebook.com/d',
-    label: 'Facebook',
-    icon: faFacebookF,
-  },
-  {
-    link: 'https://www.instagram.com/dangelosaurus/',
-    label: 'Instagram',
-    icon: faInstagram,
-  }, */
   {
     link: 'mailto:rice.brendan8+website@gmail.com',
     label: 'Email',
@@ -38,7 +22,7 @@ const data = [
   {
     link: 'https://calendar.app.google/L84NfFfh2ZAH5t259',
     label: 'Book a Meeting',
-    icon: faCalendar-check,
+    icon: fa-calendar-check,
   },
   {
     link: 'https://twitter.com/Brendan_Learn',
